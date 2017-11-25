deb998f96dab4c581a9d2bb18dc448cd6da4005a
Chris Thomas <cthomas@admt.im>
2017-11-24 21:42:28 -0700

    Disk of the Virtual Variety.

--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -7,7 +7,36 @@ namespace FileManager
     {
         public static void Main(string[] args)
         {
+            // A quick example:
             
+            var disk = new VirtualDisk();
+            disk.WriteBlock(0,new byte[1024]);
+
+            var write = new byte[1024];
+            var random = new Random();
+            random.NextBytes(write);
+            Console.WriteLine("Writing content (to block 0):");
+            for (int i = 0; i < 1024; i++)
+            {
+                Console.Write("{0:X}", write[i]);                
+            }
+            disk.WriteBlock(0, write);
+            Console.WriteLine("\n\nReading content:");
+            var data = disk.ReadBlock(0);
+            for (int i = 0; i < 1024; i++)
+            {
+                Console.Write("{0:X}", data[i]);                
+            }
+            Console.WriteLine();
+
+            if (write == disk.ReadBlock(0))
+            {
+                Console.WriteLine("They match!");
+            }
+            else
+            {
+                Console.WriteLine("They match!");
+            }
         }
     }
 }
--- FileManager/VirtualDisk.cs
+++ FileManager/VirtualDisk.cs
@@ -1,7 +1,38 @@
-﻿namespace FileManager
+﻿using System;
+
+namespace FileManager
 {
     public class VirtualDisk
     {
-        
+        private byte[] diskContents;
+
+        public VirtualDisk()
+        {
+            this.diskContents = new byte[ushort.MaxValue];
+        }
+
+        public byte[] ReadBlock(ushort block)
+        {
+            var data = new byte[1024];
+
+            for (ushort i = 0; i < 1024; i++)
+            {
+                data[i] = this.diskContents[block * 1024 + i];
+            }
+            
+            return data;
+        }
+
+        public void WriteBlock(ushort block, byte[] data)
+        {
+            if (data.Length != 1024)
+            {
+                throw new Exception("Tried to write block of size " + data.Length + ". Each block must be 1024 Bytes/1kB in size.");
+            }
+            for (ushort i = 0; i < 1024; i++)
+            {
+                this.diskContents[block * 1024 + i] = data[i];
+            }
+        }
     }
 }
