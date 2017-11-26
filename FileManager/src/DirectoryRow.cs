83082735f2d6f5b68a0d23852dc561a7beeff69a
MiriamRognlie <miriam.rognlie@gmail.com>
2017-11-25 21:56:10 -0700

    outlines for table and row classes

--- FileManager/src/DirectoryRow.cs
+++ FileManager/src/DirectoryRow.cs
@@ -2,6 +2,32 @@
 {
     public class DirectoryRow
     {
-        
+        string name = "";
+        private ushort blockStart;
+        private ushort size;
+        private ushort next;
+
+        public DirectoryRow(string n, ushort b, ushort s, ushort x)
+        {
+            this.name = n;
+            this.blockStart = b;
+            this.size = s;
+            this.next = x;
+        }
+
+        public void createFromBytes(byte[] row)//return type will be DirectoryRow
+        {
+           
+        }
+
+        public void createFromInformation(string itemName, ushort blockStart, ushort size, ushort next)//return type will be DirectoryRow
+        {
+            
+        }
+
+        public void toBytes() //return type will be byte[]
+        {
+           
+        }
     }
 }
--- FileManager/src/DirectoryTable.cs
+++ FileManager/src/DirectoryTable.cs
@@ -2,6 +2,18 @@
 {
     public class DirectoryTable
     {
-        
+        private byte[] blockData;
+        //private ArrayList rows = new ArrayList(DirectoryRow); //check syntax?
+
+
+        public void createFromBytes(byte[] data)//return type will be DirectoryTable
+        {
+            
+        }
+
+        public void toBytes()//return type will be byte[]
+        {
+            
+        }
     }
 }
