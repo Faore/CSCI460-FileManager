97cb4ba109b66fd32624149d84dce9261b94ae1e
Chris Thomas <cthomas@admt.im>
2017-11-27 13:01:47 -0700

    Directory Table ToBytes

--- FileManager/src/DirectoryTable.cs
+++ FileManager/src/DirectoryTable.cs
@@ -1,19 +1,54 @@
-﻿namespace FileManager
+﻿using System;
+using System.Collections;
+
+namespace FileManager
 {
     public class DirectoryTable
     {
-        private byte[] blockData;
-        //private ArrayList rows = new ArrayList(DirectoryRow); //check syntax?
+        private byte[] BlockData;
+        private readonly ArrayList Rows = new ArrayList();
 
+        public DirectoryTable()
+        {
+            
+        }
 
+        public DirectoryRow GetRow(byte i)
+        {
+            return (DirectoryRow) Rows[i];
+        }
+
+        public void InsertRow(DirectoryRow row)
+        {
+            if (Rows.Count == 32)
+            {
+                throw new Exception("Table cannot store anymore rows.");
+            }
+            Rows.Add(row);
+        }
+
+        public void DeleteRow(DirectoryRow row)
+        {
+            Rows.Remove(row);
+        }
+        
         public void createFromBytes(byte[] data)//return type will be DirectoryTable
         {
             
         }
 
-        public void toBytes()//return type will be byte[]
+        public byte[] toBytes()
         {
-            
+            byte[] bytes = new byte[2048];
+            for (int i = 0; i < Rows.Count; i++)
+            {
+                byte[] row = ((DirectoryRow) Rows[i]).toBytes();
+                for (int j = 0; j < 64; j++)
+                {
+                    bytes[i * 64 + j] = row[j];
+                }
+            }
+            return bytes;
         }
     }
 }
