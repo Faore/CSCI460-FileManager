50e9f4b8a50a7094cfb713d3c0d385928c6db835
MiriamRognlie <miriam.rognlie@gmail.com>
2017-11-27 13:17:03 -0700

    Return info from a diretory row as byte array

--- FileManager/src/DirectoryRow.cs
+++ FileManager/src/DirectoryRow.cs
@@ -1,6 +1,7 @@
 ﻿    using System;
-    
-    namespace FileManager
+    using System.Diagnostics.SymbolStore;
+
+namespace FileManager
     {
         public class DirectoryRow
         {
@@ -13,12 +14,12 @@
             {
                 if(n.Length >58)
                 {
-                    throw new Exception("File name too long");
+                    throw new Exception("File name too long");//cannot fit names strings larger than 58 chars into row name
                 }
                 
                 char[] n2 = n.ToCharArray();
                 char[] n3 = new char[58];
-                for (int i = 0; i <n2.Length; i++)
+                for (int i = 0; i <n2.Length; i++)//creates char array from input string n, padded with nulls to be length 58
                 {
                     n3[i] = n2[i];
                 }
@@ -29,7 +30,7 @@
                 this.next = x;
             }
     
-            public static DirectoryRow createFromBytes(byte[] row)//return type will be DirectoryRow
+            public static DirectoryRow createFromBytes(byte[] row)//crate a new directry row based on an input array of bytes
             {
                 string n = "";
                 ushort b = 0;
@@ -46,12 +47,30 @@
                 
                 x = (ushort) (row[62] + (row[63] << 8));//parse bit 63 and 64 in "row", this corresponds to file's next
                 
-                return new DirectoryRow(n, b, s, x);
+                return new DirectoryRow(n, b, s, x);//create new instance and return it
             }
     
-            public void toBytes() //return type will be byte[]
+            public byte[] toBytes() //takes all 4 items in a directory row instance, cast them to bytes and return all as a byte array of length 64
             {
-               
+                byte[] row = new byte[64];
+                for (int i = 0; i < 58; i++)
+                {
+                    row[i] = (byte)name[i];
+                }
+                byte b1 = (byte)(blockStart & 255);//ushorts cast as 2 bytes, need to esparate before adding to return array
+                byte b2 = (byte)(blockStart >> 8);
+                row[58] = b1;
+                row[59] = b2;
+                b1 = (byte)(size & 255);
+                b2 = (byte)(size >> 8);
+                row[60] = b1;
+                row[61] = b2;
+                b1 = (byte)(next & 255);
+                b2 = (byte)(next >> 8);
+                row[62] = b1;
+                row[63] = b2;
+
+                return row;
             }
         }
     }
