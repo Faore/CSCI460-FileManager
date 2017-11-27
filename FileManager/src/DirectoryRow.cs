cff6ce18ee4d99d00ff29c226f2478abe9d05761
MiriamRognlie <miriam.rognlie@gmail.com>
2017-11-27 12:58:28 -0700

    Create new table rows from a byte array or from string/short inputs

--- FileManager/src/DirectoryRow.cs
+++ FileManager/src/DirectoryRow.cs
@@ -1,33 +1,57 @@
-﻿namespace FileManager
-{
-    public class DirectoryRow
+﻿    using System;
+    
+    namespace FileManager
     {
-        string name = "";
-        private ushort blockStart;
-        private ushort size;
-        private ushort next;
-
-        public DirectoryRow(string n, ushort b, ushort s, ushort x)
+        public class DirectoryRow
         {
-            this.name = n;
-            this.blockStart = b;
-            this.size = s;
-            this.next = x;
-        }
-
-        public void createFromBytes(byte[] row)//return type will be DirectoryRow
-        {
-           
-        }
-
-        public void createFromInformation(string itemName, ushort blockStart, ushort size, ushort next)//return type will be DirectoryRow
-        {
-            
-        }
-
-        public void toBytes() //return type will be byte[]
-        {
-           
+            char[] name;
+            private ushort blockStart;
+            private ushort size;
+            private ushort next;
+    
+            public DirectoryRow(string n, ushort b, ushort s, ushort x)
+            {
+                if(n.Length >58)
+                {
+                    throw new Exception("File name too long");
+                }
+                
+                char[] n2 = n.ToCharArray();
+                char[] n3 = new char[58];
+                for (int i = 0; i <n2.Length; i++)
+                {
+                    n3[i] = n2[i];
+                }
+                
+                this.name = n3;
+                this.blockStart = b;
+                this.size = s;
+                this.next = x;
+            }
+    
+            public static DirectoryRow createFromBytes(byte[] row)//return type will be DirectoryRow
+            {
+                string n = "";
+                ushort b = 0;
+                ushort s = 0;
+                ushort x = 0;
+                for (int i = 0; i < 58; i++)//parse first 58 bits in "row", this corresponds to file's name
+                {
+                    char character = (char) row[i];
+                    n += character;
+                }
+                b = (ushort) (row[58] + (row[59] << 8));//parse bits 59-60 in "row", this corresponds to file's blockStart
+                
+                s = (ushort) (row[60] + (row[61] << 8));//parse bits 61-62 in "row", this corresponds to file's size
+                
+                x = (ushort) (row[62] + (row[63] << 8));//parse bit 63 and 64 in "row", this corresponds to file's next
+                
+                return new DirectoryRow(n, b, s, x);
+            }
+    
+            public void toBytes() //return type will be byte[]
+            {
+               
+            }
         }
     }
-}
